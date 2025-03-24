a509a1b42506a6e0ec648672d6d06dbab6898125
Chengcheng Jin <Chengcheng.jin@ibm.com>
2025-03-24 11:15:42 +0000

    update files

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -160,7 +160,8 @@ if [ -d $VELOX_SOURCE_DIR ]; then
     fi
     git reset --hard HEAD
     git checkout refs/tags/build_$TARGET_BUILD_COMMIT
-    git fetch origin pull/12735/head
+    git remote add open https://github.com/facebookincubator/velox.git
+    git fetch open pull/12735/head
     git rebase FETCH_HEAD
   else
     echo "$VELOX_BRANCH can't be found in $VELOX_REPO, skipping the download..."
@@ -169,7 +170,8 @@ else
   git clone $VELOX_REPO -b $VELOX_BRANCH $VELOX_SOURCE_DIR
   cd $VELOX_SOURCE_DIR
   git checkout $TARGET_BUILD_COMMIT
-  git fetch origin pull/12735/head
+  git remote add open https://github.com/facebookincubator/velox.git
+  git fetch open pull/12735/head
   git rebase FETCH_HEAD
 fi
 
