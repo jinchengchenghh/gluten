9615654d0bf190aa4e201c90f09dcc4e577235fb
Chengcheng Jin <Chengcheng.jin@ibm.com>
2025-05-12 14:27:19 +0100

    update velox branch

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -16,8 +16,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=2025_05_12
+VELOX_REPO=https://github.com/jinchengchenghh/velox.git
+VELOX_BRANCH=benchmark
 VELOX_HOME=""
 
 OS=`uname -s`
