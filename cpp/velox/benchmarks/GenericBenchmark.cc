f338a66cd9ef078f5a477cd1911ebfba43761d8b
Chengcheng Jin <Chengcheng.jin@ibm.com>
2025-05-29 11:00:42 +0800

    benchmark enable cudf

--- cpp/velox/benchmarks/GenericBenchmark.cc
+++ cpp/velox/benchmarks/GenericBenchmark.cc
@@ -426,8 +426,9 @@ auto BM_Generic = [](::benchmark::State& state,
       std::vector<FileReaderIterator*> inputItersRaw;
       if (!dataFiles.empty()) {
         for (const auto& input : dataFiles) {
-          inputIters.push_back(FileReaderIterator::getInputIteratorFromFileReader(
-              readerType, input, FLAGS_batch_size, runtime->memoryManager()->getLeafMemoryPool().get()));
+          inputIters.push_back(
+              FileReaderIterator::getInputIteratorFromFileReader(
+                  readerType, input, FLAGS_batch_size, runtime->memoryManager()->getLeafMemoryPool().get()));
         }
         std::transform(
             inputIters.begin(),
@@ -443,7 +444,8 @@ auto BM_Generic = [](::benchmark::State& state,
         runtime->parseSplitInfo(reinterpret_cast<uint8_t*>(split.data()), split.size(), std::nullopt);
       }
 
-      auto resultIter = runtime->createResultIterator(veloxSpillDir, std::move(inputIters), runtime->getConfMap(), false);
+      auto resultIter = runtime->createResultIterator(
+          veloxSpillDir, std::move(inputIters), runtime->getConfMap(), Flags_cudf_enabled);
       listenerPtr->setIterator(resultIter.get());
 
       if (FLAGS_with_shuffle) {
