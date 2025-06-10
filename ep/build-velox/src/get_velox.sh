86df22340c0241123444967aeb3b33b1b54f9066
Chengcheng Jin <Chengcheng.jin@ibm.com>
2025-06-10 09:50:17 +0800

    update velox branch

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -16,8 +16,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=2025_06_08
+VELOX_REPO=https://github.com/jinchengchenghh/velox.git
+VELOX_BRANCH=gpu_610
 VELOX_HOME=""
 RUN_SETUP_SCRIPT=ON
 
