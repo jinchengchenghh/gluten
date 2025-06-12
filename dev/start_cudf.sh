fd4f5e0848fc55cf368e67a8556048f9933fb032
Chengcheng Jin <Chengcheng.jin@ibm.com>
2025-06-12 09:37:00 +0800

    Update to redhat cuda tool kit

--- dev/start_cudf.sh
+++ dev/start_cudf.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 # Execute this script in host machine
+#!/bin/bash
+# Execute this script in host machine
 set -eu
 
-wget https://developer.download.nvidia.com/compute/cuda/12.8.1/local_installers/cuda-repo-amzn2023-12-8-local-12.8.1_570.124.06-1.x86_64.rpm
-sudo rpm -i cuda-repo-amzn2023-12-8-local-12.8.1_570.124.06-1.x86_64.rpm
+wget https://developer.download.nvidia.com/compute/cuda/12.9.1/local_installers/cuda-repo-rhel9-12-9-local-12.9.1_575.57.08-1.x86_64.rpm
+sudo rpm -i cuda-repo-rhel9-12-9-local-12.9.1_575.57.08-1.x86_64.rpm
 sudo dnf clean all
-sudo dnf -y install cuda-toolkit-12-8
-sudo dnf -y module install nvidia-driver:open-dkms
-
-sudo dnf install -y kernel-modules-extra
-sudo dnf install -y kernel-devel-$(uname -r) gcc make dkms
-sudo dkms autoinstall
-sudo modprobe nvidia
-sudo nvidia-smi
-
-sudo yum install -y docker
+sudo dnf -y install cuda-toolkit-12-9
 
 curl -s -L https://nvidia.github.io/libnvidia-container/stable/rpm/nvidia-container-toolkit.repo | \
   sudo tee /etc/yum.repos.d/nvidia-container-toolkit.repo
 sudo dnf install -y nvidia-container-toolkit
-sudo nvidia-ctk runtime configure --runtime=docker
-sudo systemctl restart docker
-# May need reboot here after install cuda driver
-# Run the gpu example
-sudo docker run --rm --runtime=nvidia --gpus all ubuntu nvidia-smi
-# Then run this command to
-sudo docker run --name gpu_gluten_container --gpus all -itd apache/gluten:centos-9-jdk8-cudf
+
+sudo dnf install -y epel-release
+sudo dnf update -y
+sudo dnf install -y dkms
+sudo dnf config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel9/x86_64/cuda-rhel9.repo
+sudo dnf install -y cuda nvidia-driver
+sudo dkms status
+sudo reboot
+
+# after reboot
+#sudo dkms build nvidia/575.57.08 && sudo dkms install nvidia/575.57.08
+#sudo nvidia-ctk runtime configure --runtime=docker
+#sudo systemctl restart docker
+#sudo docker run --rm --runtime=nvidia --gpus all ubuntu nvidia-smi
