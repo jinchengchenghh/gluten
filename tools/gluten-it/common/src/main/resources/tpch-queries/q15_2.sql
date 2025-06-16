70d5f470b983a802ab5f8f24340928738a3c7aca
Chengcheng Jin <Chengcheng.jin@ibm.com>
2025-06-16 10:41:02 +0800

    test q15

---
+++ tools/gluten-it/common/src/main/resources/tpch-queries/q15_2.sql
@@ -0,0 +1,16 @@
+with revenue0 as
+         (select
+              l_suppkey as supplier_no,
+              sum(l_extendedprice * (1 - l_discount)) as total_revenue
+          from
+              lineitem
+          where
+              l_shipdate >= date '1996-01-01'
+            and l_shipdate < date '1996-01-01' + interval '3' month
+          group by
+              l_suppkey)
+
+select
+    max(total_revenue)
+from
+    revenue0
